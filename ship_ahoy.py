e96600d6f0aa7f590f51dea0505482be18239518
Erik Bryant <erik_bryant@yahoo.com>
2018-04-27 21:42:49 -0700

    Add the ability to alert on ships that are just coming into visible range.

--- ship_ahoy.py
+++ ship_ahoy.py
@@ -1,3 +1,4 @@
+import json
 import time
 import urllib.request
 import pygame
@@ -12,10 +13,16 @@ import pygame
 # Put exception handling around url calls.
 #  socket.gaierror: [Errno -2] Name or service not known
 #  urllib.error.URLError: <urlopen error [Errno -2] Name or service not known>
+# Write the detected ships to a database so we can keep stats on them.
 
 
 ShipsSeen = {}
 ExpireSecs = 60
+
+Visible_latA  = 37.8052
+Visible_latB  = 37.8613
+Visible_longA = -122.4844
+Visible_longB = -122.4092
 
 
 # alert() prints a message and plays an alert tone.
@@ -72,16 +79,43 @@ def mmsi_details(mmsi):
 
     url = "https://www.vesselfinder.com/clickinfo?mmsi=%s&rn=64229.85898456942&_=1524694015667" % mmsi
     req = urllib.request.Request(url, headers=headers)
-    content_raw = b''
-    with urllib.request.urlopen(req) as response:
-        content_raw += response.read()
-
-    return content_raw.decode('utf-8')
+    content_raw = urllib.request.urlopen(req)
+    return json.load(content_raw)
 
 
-# interesting() determines which ships in a given list are of interest. If any are,
-# it signals an alert.
-def interesting(ships, headingMin=0, headingMax=359):
+# visible_from_apt() returns a bool indicating whether the ship is visible
+# from our apartment window.
+def visible_from_apt(lat1, long1):
+    # Note that A is the bottom left corner and B is the upper
+    # right corner, so we need to work out C and D which are the
+    # upper left and lower right corners.
+    latC  = Visible_latB
+    latD  = Visible_latA
+    longC = Visible_longA
+    longD = Visible_longB
+
+    # Is the ship within the bounding box of our visible area?
+    if lat1 < latD or lat1 > latC:
+        return False
+    if long1 < longC or long1 > longD:
+        return False
+
+    # Is the ship within our visible triangle (the bottom left
+    # triangle of the bounding box)? It is if the latitude is
+    # less than the latitude of the box's diagonal.
+    # x == longitude, y == latitude
+    m = (latC - latD) / (longC - longD)
+    b = latC - m*longC
+    y = m*long1 + b
+    if lat1 > y:
+        return False
+
+    return True
+
+
+# interesting() determines which ships in a given list are of interest.
+# If any are, it signals an alert.
+def interesting(ships, headingMin=0, headingMax=360, visible=False):
     uninteresting_ais = [
         '0',   # Unknown
         '6',   # Passenger
@@ -95,6 +129,8 @@ def interesting(ships, headingMin=0, headingMax=359):
     uninteresting_mmsis = [
         '367123640',  # Hawk
         '367389640',  # Oski
+        '366990520',  # Del Norte
+        '367566960',  # F/V Pioneer
     ]
 
     for ship in ships.split('\n'):
@@ -104,39 +140,51 @@ def interesting(ships, headingMin=0, headingMax=359):
         if len(fields) < 2:
             continue
 
-        heading = int(fields[2]) / 10.0
-        speed   = int(fields[3]) / 10.0
+        # https://api.vesselfinder.com/docs/response-ais.html
+        lat1    = int(fields[0]) / 600000.0
+        long1   = int(fields[1]) / 600000.0
+        course  = int(fields[2]) / 10.0
+        speed   = int(fields[3]) / 10.0  # SOG
         ais     = fields[4]
         mmsi    = fields[5]
         name    = fields[6]
 
-        # Only look at ships that are moving relatively fast.
-        if speed < 7:
-            # print("Skipping by speed: %s" % ship)
+        # Only look at ships that are moving.
+        if speed < 5:
             continue
 
-        # Only look at inbound ships.
-        if heading < headingMin or heading > headingMax:
-            # print("Skipping by heading: %s" % ship)
+        # Only look at ships headed the direction of interest.
+        if course < headingMin or course > headingMax:
+            continue
+
+        # Only look at ships visible from our apartment?
+        if visible and not visible_from_apt(lat1, long1):
             continue
 
         # Skip 'uninteresting' ships.
         if ais in uninteresting_ais:
-            # print("Skipping uninteresting AIS: %s" % ship)
             continue
         if mmsi in uninteresting_mmsis:
-            # print("Skipping uninteresting MMSI: %s" % ship)
             continue
 
+        url = "https://www.vesselfinder.com/?mmsi=%s&zoom=13" % mmsi
         details = mmsi_details(mmsi)
-        alert(mmsi, "Ship ahoy!  %s\n%s" % (ship, details))
+        del details['imo']
+        del details['etastamp']
+        del details['ship_speed']
+        del details['ship_course']
+        del details['timestamp']
+        del details['direct_link']
+        del details['pn']
+        alert(mmsi, "Ship ahoy!  %s\n%s\n%s\n%f, %f\n" % (ship, details, url, lat1, long1))
 
 
 def main():
+    # Initialize the sound system.
     pygame.mixer.init()
 
     # The part of the bay visible from our apartment.
-    visible = "https://www.vesselfinder.com/vesselsonmap?bbox=-122.50628910495868%2C37.7868980951191%2C-122.37668476535909%2C37.87150340418255&zoom=13&mmsi=0&show_names=1&ref=10800.429711736251&pv=6"
+    visible = "https://www.vesselfinder.com/vesselsonmap?bbox=%f%%2C%f%%2C%f%%2C%f&zoom=13&mmsi=0&show_names=1&ref=76653.332556956&pv=6" % (Visible_longA, Visible_latA, Visible_longB, Visible_latB)
 
     # The part of the bay to the west of our visible area.
     gate = "https://www.vesselfinder.com/vesselsonmap?bbox=-122.56280995055705%2C37.77840105911834%2C-122.47822461224163%2C37.833635454273335&zoom=13.615634506295129&mmsi=0&show_names=1&ref=53552.672128591235&pv=6"
@@ -145,13 +193,17 @@ def main():
     outbound = "https://www.vesselfinder.com/vesselsonmap?bbox=-122.45043142336208%2C37.79005643280233%2C-122.36597402590112%2C37.94129487900324&zoom=12&mmsi=0&show_names=1&ref=35521.28976544603&pv=6"
 
     while True:
-        # Inbound ships about to enter our visible area.
-        ships = ships_by_region(gate)
-        interesting(ships=ships, headingMin=10, headingMax=170)
+        # Ships in our visible area.
+        ships = ships_by_region(visible)
+        interesting(ships=ships, visible=True)
+        
+        # # Inbound ships about to enter our visible area.
+        # ships = ships_by_region(gate)
+        # interesting(ships=ships, headingMin=10, headingMax=170)
 
-        # Outbound ships about to enter our visible area.
-        ships = ships_by_region(outbound)
-        interesting(ships=ships, headingMin=225, headingMax=315)
+        # # Outbound ships about to enter our visible area.
+        # ships = ships_by_region(outbound)
+        # interesting(ships=ships, headingMin=225, headingMax=315)
 
         # Do not spam their web service.
         time.sleep(ExpireSecs)
