37a4bd009b246f9fc8867349da31ffc44672e90a
Erik Bryant <erik_bryant@yahoo.com>
2018-07-17 00:06:39 -0700

    Loop across the planet in 2 degree increments. Instead of trying to get an entire continent at a time, simply sweep across the globe in 2 degree increments.
    Add a table for ship sightings.
    
    Move the SQL backups to a subdirectory.
    There is now a sightings table. Back that up, too.

--- ship_ahoy.py
+++ ship_ahoy.py
@@ -1,11 +1,11 @@
 import argparse
-import geocoder
 import json
 import time
 import urllib.request
-import pygame
 
+import geocoder
 import mysql.connector
+import pygame
 
 
 # Most recent ports of call.
@@ -30,7 +30,7 @@ import mysql.connector
 #  socket.gaierror: [Errno -2] Name or service not known
 #  urllib.error.URLError: <urlopen error [Errno -2] Name or service not known>
 # Put exception handling around geo calls.
-# $ python3 ship_ahoy.py 
+# $ python3 ship_ahoy.py
 #  Status code Unknown from http://ipinfo.io/json: ERROR - HTTPConnectionPool(host='ipinfo.io', port=80): Max retries exceeded with url: /json (Caused by NewConnectionError('<urllib3.connection.HTTPConnection object at 0x7f2df411f8d0>: Failed to establish a new connection: [Errno -2] Name or service not known',))
 #  Traceback (most recent call last):
 #    File "ship_ahoy.py", line 321, in <module>
@@ -40,6 +40,65 @@ import mysql.connector
 #    File "ship_ahoy.py", line 86, in bbox
 #      lat = geo.latlng[0]
 #  TypeError: 'NoneType' object is not subscriptable
+#
+
+# Data Requests
+#
+# Ships in a region
+# 22235849   -- lat * 600000
+# 522        -- lon * 600000
+# 683        -- Course *10
+# 117        -- Speed * 10
+# 280        -- ais
+# 249857000  -- mmsi
+# WAIKIKI    -- Ship name
+# 0          -- unknown
+#
+# MMSI data of a given ship
+# {
+#  'imo': '9776755',
+#  'name': 'WAIKIKI',
+#  'type': 'Crude Oil Tanker',
+#  't': '1531634521',
+#  'sar': False,
+#  'dest': 'MALTA',
+#  'etastamp': 'Jul 19, 12:00',
+#  'ship_speed': 11.7,
+#  'ship_course': 68.3,
+#  'timestamp': 'Jul 15, 2018 06:02 UTC',
+#  '__id': '309251',
+#  'pn': '9776755-249857000-6d17e98fedf50ed074675bd8f3396cd5',
+#  'vo': 0,
+#  'ff': False,
+#  'direct_link': '/vessels/WAIKIKI-IMO-9776755-MMSI-249857000',
+#  'draught': 8.8,
+#  'year': '2017',
+#  'gt': '61468',
+#  'sizes': '250 x 44 m',
+#  'dw': '112829'
+# }
+#
+# Data query to ship_ahoy.ships
+# {
+#  'mmsi': '374518000',
+#  'imo': '8687218',
+#  'name': 'DONG HONG HANG 2',
+#  'ais': 170,
+#  'type': 'Bulk Carrier',
+#  'sar': 0,
+#  '__id': '0',
+#  'vo': 0,
+#  'ff': 0,
+#  'direct_link': '/vessels/DONG-HONG-HANG-2-IMO-8687218-MMSI-374518000',
+#  'draught': 5.0,
+#  'year': 2011,
+#  'gt': 8465,
+#  'sizes': '137 x 20 m',
+#  'length': 137,
+#  'beam': 20,
+#  'dw': 13685,
+#  'unknown': 0
+# }
 #
 
 # SQL statements
@@ -81,14 +140,17 @@ import mysql.connector
 
  CREATE TABLE sightings (
     mmsi varchar(20),
-    dest varchar(128),
     ship_course float,
-    timestamp datetime,
+    timestamp int,  # Unix datetime
     lat float,
     lon float,
     my_lat float,
-    my_lon float,
+    my_lon float
  );
+
+ # Backup / Restore
+ mysqldump -u root -p db_name t1 > dump.sql
+ mysql -u root-p db_name < dump.sql
 
 """
 
@@ -118,24 +180,39 @@ KEYS = [
     'dw',
     'unknown',
 ]
+KEYS_SIGHTING = [
+    'mmsi',
+    'ship_course',
+    'timestamp',
+    'lat',
+    'lon',
+    'my_lat',
+    'my_lon',
+]
+
+
+# my_location() returns the geo coordinates of where this
+# computer is as a (lat, lon) pair.
+def my_location():
+    geo = geocoder.ip('me')
+    return (geo.latlng[0], geo.latlng[1])
 
 
 # bbox() returns a bounding box of the circle with center of the
 # current location and radius of 'nmiles' nautical miles.
 # Returns (longA, latA, longB, latB) Where A is the bottom left
 # corner and B is the upper right corner.
-def bbox(nmiles=15):
+def bbox(nmiles, latlon):
     # Convert nautical miles to decimal degrees.
     delta = nmiles * 1.0 / 60.0
 
-    geo = geocoder.ip('me')
-    lat = geo.latlng[0]
-    lng = geo.latlng[1]
+    lat = latlon[0]
+    lon = latlon[1]
 
     bbox_latA = lat - delta
-    bbox_longA = lng - delta
+    bbox_longA = lon - delta
     bbox_latB = lat + delta
-    bbox_longB = lng + delta
+    bbox_longB = lon + delta
 
     return (bbox_longA, bbox_latA, bbox_longB, bbox_latB)
 
@@ -143,14 +220,6 @@ def bbox(nmiles=15):
 # alert() prints a message and plays an alert tone.
 # Mute if we have already seen this ship.
 def alert(mmsi='', ship='', details={}, url=''):
-    now = time.time()
-    last_seen = ShipsSeen.get(mmsi, 0)
-    mute = now - last_seen <= ExpireSecs
-    expire = now + 2*ExpireSecs
-    ShipsSeen[mmsi] = expire
-    if mute:
-        return
-
     print("Ship Ahoy!   %s\n%s\n%s\n" % (ship, details, url))
 
     # Play an alert tone.
@@ -169,13 +238,43 @@ def alert(mmsi='', ship='', details={}, url=''):
     pygame.mixer.quit()
 
 
-# persist() saves a ship sighting to the database.
-def persist(details):
+# persist_ship() saves a ship's data to the database.
+def persist_ship(details):
     INSERT = "INSERT IGNORE INTO ships ( %s )" % ','.join(KEYS)
     INSERT += " VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s )"
 
     row = []
     for key in KEYS:
+        value = ''
+        if key in details:
+            value = details[key]
+        row.append(value)
+
+    cnx = mysql.connector.connect(user='root', password='password', database='ship_ahoy')
+    cursor = cnx.cursor()
+    cursor.execute(INSERT, row)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+
+# persist_sighting() saves a ship sighting to the database.
+def persist_sighting(mmsi, ship_course, lat, lon):
+    INSERT = "INSERT INTO sightings ( %s )" % ','.join(KEYS_SIGHTING)
+    INSERT += " VALUES ( %s, %s, %s, %s, %s, %s, %s )"
+
+    details = {}
+    details['mmsi'] = mmsi
+    details['ship_course'] = ship_course
+    details['timestamp'] = int(time.time())
+    details['lat'] = lat
+    details['lon'] = lon
+    latlon = my_location()
+    details['my_lat'] = latlon[0]
+    details['my_lon'] = latlon[1]
+
+    row = []
+    for key in KEYS_SIGHTING:
         value = ''
         if key in details:
             value = details[key]
@@ -276,16 +375,16 @@ def web_request(url='', use_json=False):
 # from our apartment window.
 def visible_from_apt(lat1, long1):
     # The bounding box for the area visible from our apartment.
-    Visible_latA  = 37.8052
-    Visible_latB  = 37.8613
+    Visible_latA = 37.8052
+    Visible_latB = 37.8613
     Visible_longA = -122.48
     Visible_longB = -122.4092
 
     # Note that A is the bottom left corner and B is the upper
     # right corner, so we need to work out C and D which are the
     # upper left and lower right corners.
-    latC  = Visible_latB
-    latD  = Visible_latA
+    latC = Visible_latB
+    latD = Visible_latA
     longC = Visible_longA
     longD = Visible_longB
 
@@ -341,13 +440,13 @@ def interesting(ships):
             continue
 
         # https://api.vesselfinder.com/docs/response-ais.html
-        lat1    = int(fields[0]) / 600000.0
-        long1   = int(fields[1]) / 600000.0
-        course  = int(fields[2]) / 10.0
-        speed   = int(fields[3]) / 10.0  # SOG
-        ais     = int(fields[4])
-        mmsi    = fields[5]
-        name    = fields[6]
+        lat = int(fields[0]) / 600000.0
+        lon = int(fields[1]) / 600000.0
+        ship_course = int(fields[2]) / 10.0
+        speed = int(fields[3]) / 10.0  # SOG
+        ais = int(fields[4])
+        mmsi = fields[5]
+        name = fields[6]
         unknown = int(fields[7])
 
         url = "https://www.vesselfinder.com/?mmsi=%s&zoom=13" % mmsi
@@ -359,21 +458,20 @@ def interesting(ships):
             print("Found new ship: %s %s" % (mmsi, name))
             mmsi_url = "https://www.vesselfinder.com/clickinfo?mmsi=%s&rn=64229.85898456942&_=1524694015667" % mmsi
             details = web_request(url=mmsi_url, use_json=True)
+            if details == '':
+                continue
             length = 0
             beam = 0
-            try:
-                sizes = details['sizes'].split(' ')
-                if len(sizes) == 4 and sizes[1] == 'x' and sizes[3] == 'm':
-                    length = int(sizes[0])
-                    beam = int(sizes[2])
-            except:
-                print("ERROR:", details['sizes'])
+            sizes = details['sizes'].split(' ')
+            if len(sizes) == 4 and sizes[1] == 'x' and sizes[3] == 'm':
+                length = int(sizes[0])
+                beam = int(sizes[2])
             details['mmsi'] = mmsi
             details['ais'] = ais
             details['length'] = length
             details['beam'] = beam
             details['unknown'] = unknown
-            persist(details=details)
+            persist_ship(details=details)
         else:
             if details['ais'] != ais:
                 update(mmsi=mmsi, col='ais', value=ais)
@@ -389,9 +487,18 @@ def interesting(ships):
             continue
 
         # Only alert for ships visible from our apartment.
-        if not visible_from_apt(lat1, long1):
+        if not visible_from_apt(lat, lon):
             continue
 
+        now = time.time()
+        last_seen = ShipsSeen.get(mmsi, 0)
+        mute = now - last_seen <= 5*ExpireSecs
+        ShipsSeen[mmsi] = now
+        if mute:
+            continue
+
+        # We have passed all the tests! Save and alert.
+        persist_sighting(mmsi, ship_course, lat, lon)
         alert(mmsi, ship, details, url)
 
 
@@ -400,24 +507,34 @@ def main():
     parser.add_argument('--snapshot', help='exit after one scan pass', action='store_true')
     args = parser.parse_args()
 
+    boxes = []
     # (longA, latA, longB, latB) Where A is the bottom left
     # corner and B is the upper right corner.
-    box = (0, -16, 160, 62)  # Europe, SE Asia
-    box = bbox(nmiles=150)
-    box = (-193, -16, -36, 71)  # North America
-    url = "https://www.vesselfinder.com/vesselsonmap?bbox=%f%%2C%f%%2C%f%%2C%f" % box
-    url += "&zoom=12&mmsi=0&show_names=1&ref=35521.28976544603&pv=6"
+    # boxes.append((-193, -16, -36, 71))  # North America
+    # boxes.append((0, -16, 160, 62))  # Europe, SE Asia
+
+    step = 2
+    for i in range(0, 180, step):
+        boxes.append(bbox(nmiles=100, latlon=my_location()))
+        boxes.append((i, -80, i+step, 80))
+        boxes.append(bbox(nmiles=100, latlon=my_location()))
+        boxes.append((-i, -80, -(i+step), 80))
 
     while True:
-        print("Scanning...")
-        ships = web_request(url=url)
-        interesting(ships=ships)
+        for box in boxes:
+            print("Scanning", box)
 
-        if args.snapshot:
-            break
+            url = "https://www.vesselfinder.com/vesselsonmap?bbox=%f%%2C%f%%2C%f%%2C%f" % box
+            url += "&zoom=12&mmsi=0&show_names=1&ref=35521.28976544603&pv=6"
 
-        # Do not spam their web service.
-        time.sleep(ExpireSecs)
+            ships = web_request(url=url)
+            interesting(ships=ships)
+
+            if args.snapshot:
+                return
+
+            # Do not spam their web service.
+            time.sleep(ExpireSecs)
 
 
 main()
