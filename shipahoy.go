5fb1b98d1512c4f9789e72ddb1c43788b4813d64
Erik Bryant <erikbryantology@gmail.com>
2020-07-19 10:07:59 -0700

    Refactor to simplify/reduce code
    Have all of the scanners make use of lookAtShips() instead of
    implementing their own channels/loops.

--- shipahoy.go
+++ shipahoy.go
@@ -20,9 +20,6 @@ var (
 
 	gcpAuthCrypt = "GXHhVSPiA/XlDKH+EV6RBxeLn6qvQ92A1y1CzLiTFs9y2qAaUBkUUNYzt4vBANh4Hojd00MYwj4d0lHnBe2lonj6pljVBQKCr/KvNWzG/BQjkb71QZ0IxGD1q5si0613ol6s6zOGO/c/WbBwPLdSoGv6tg2yIAVDIGACqEjCvyWwx7jmc808Gi9xOUI629WJBRydcSKG0/P9mbGePRyrnfuQw5051tKoI6xNyDlnly/gh4CsR46LyjDM1+E8kXTLUOXP+rg0YGPAbDEmLdFfbdVMcZQjYDijwUlXPucj/Q1voCPd/T/zxEGgM5nLFC1HWnAO0wzeCaDh3EUsS0R9FFi9l+ut6ekvFw1HZiLv49uR++vglYl01vW3bdG/P+DVlz+MF7s6kLDUftSWPfzIlI8rw7xd1UmNKSWTLik5JLrU4JeRfVBClyKvFKtTFqVv9HJXS0KL1wPXyvVtJkxgbcwhzsqHxVqdopvx1i674fovBMIv2oN65RczTTqRxLZXcDM9oMontd5+1QvmcV4QQVkM3ph4qLVb9mwnMEcSkAJ1/Qq0KO55Su96WR3QKoUC2VQiBDsYlLnc4uUVxNBpiYl4NW3GQTLarVyfe+RtxfhRtUweVRaJDcVZ7MFdWT3TCxnV3hdlLD6QblS0Fz+fifkKgjVd8U4TSAlDWKCUi1HI/Oq0UjlLb19d8WifL/rLeBKgQnBzsY0FV02P5rkGozxlfZFklCp0cH3j9sCYCypfC+XkJlgb4IJmjcWPnbuPup9Hpw4any62lZvo/sit05MB77c6Q8HloHy96MGXdzmit56vzgI7ZBJcFqi6TOqDVl06QJxsKu5tguscbJw6LlasVyf1rWqck9UQKMmP6ZQqg1Zgaw7auc04QN/0KfiYu5IShAYQuN6MEC5Ibrr7SBL5lWUtlSnQ6Ywgnti0GCuCZ1DsrFsUUVbHFf8IJKvvrL5CmiAJOe6WV+XXRbyKA/bjMjqkS1V9UlmTHPzEbhZ3cEWDODlekSrqTdG+fU2136LS/b8bWtNDzl6kodqbUr7l70FxV195bnGhSCFw6ciiuYdYGcC9lb22KkOGHNnE8WSPT4+QsK9yyOCrgPBMSJtA7XKINE/+szQ0giRgCo4HJH+KqE08vIZOLzMrZdCSTDe5HqvLPJ4CjngsMwjlgBhhmqBsgJcL4GVU1ONpD6o1zT4RGendz7KU1VOa1llBw59xZjN5gx+2GdkEGOltBECdxeALxU8lhQbpg/7ZKXyGhAcDIAXDSxPBl8Xz64aAFi2TmL8OV5inB+GrxseUjAqyythf4gOUyxMkE1gxuS1qyB6EzH49Y38xBmj2Jo274e52Ubs4ceuiDDktCiEFGg3OYrqOcIScM/2Y3YiICtlfqFLzzL2379G5ZUaHgTrve2OWWHU/5PpJ1oUvMrzwVJbutyNspAzYZrt8gjvRsf18i1nOenWHNXbvwOqVV6uJ/5xVZWn8B37pR0F+VzaB2VRVrqI4udHvgFoAjUvz+xH+wsMAby0PKGWBY3Z2fRR+VNAZouCn/S6HH8m4lDRibiHX6pGYszMMvA+LBDE7m/SSQ73zjm1gvxTHmSyb8RsFPV1cgOprya1y95M8epe706Lz3tEJ7Szd4c2Mbj5B/5NoxOw+3dZM9xXc4NqPwoB/ZdyDgTp2lPTuVhfI8zy9SjnXXPMLAQxh82cKfyqKoWA/2PjR3a2d6TreeEQJVDP39nGXnajxhYyXnHJlaO2xSIeuWw7Qu/ChLRNN6ORlT1OG4bEGwetgq+87ShmFidZVOu4QnG8QBpSqqfLv1lLJ/prAl3T4ghCv8zJKlRtRxk/mFa2mjJi8lRC7rlhayJkyJTKdRGZTDrfb1ObnEx109VuCkBEiBsY0pExeI/DbH0diWjPx6iDK1MTzhQe+v3tvHNY7yYejE48MZ9LVfeyj7FRO6oVozWn03ZmN4BO/LPHoy8duqbUJUnvsiizjv9VR1P5ken+WVshqRwAx18ryedDXZOTwXeO1BgnYU/1Th7aouB9JAPCc1KJfUTju0r0zdQSIXr4LhJICpZJ3ff2EERLiXqOsumFFGxEKJVc4KR/Xw+f8seuAtXUsE/xdRmcJP3X6G+NGkRk4Bie/QiGSjgPvwTh+VpRiTFozRBLXQhLsW1+0dDd0VZ/Jem8b0CiReXolivK6OI4oW1d1GDgtw4Vm0E98PR6saQNW35LYJ6mDvK+pgllnKwR/8oIp9JyUmGeZRXg4UR9h2oe626Wdr3AmMVkYqlqUfwVMrmU3GMxNnyoIiTEkrjYhtkKbNX+Bfog0epSsdMJoiyDPq1op+Cg3CnnmTqRIbT4T03ZXOOMVS3dHeUF9BcVnznhwja8DttWeJtGXmDbSd5E3gaOeOqeXh1OCZeX2WiyDudPO8MlRVOIq/n9g1i7eDg1JQ8xe54Jb9mJBlLwN9wmp5Ux5+N/9cmGkk/0zKoDVsskJFWHSfysg4CyEOBF6yjRVr0kW0w0bZmhZtdUV6DukoxNWsP2+XNDiCmBk9cgruzxdupg8qP3WAYJ23ahz0vNjU2pIBoY6PIouLFyCYAimQfLhuYgxzXZ3KIyvXjTjTpqV15u5XUVsozCKRX2BbFa795Kzxxb/BH/GYNSpdmaD4l3OkvfowpTMVx/6yK05txPY+hQcRRq1NNrBHB0MQ3XYHt7cTaCMLIpKuDjCEgz8B34M5I5QMHHKGuu7PXxzkw57gbukHNVbvCCwGv6HLj47hy0fD7KoRt3UM7h0Lq+/1AiysExkM/nXy9xROzCMrBIRx2ymDpXBTP24SB44orDB5j0h1JYX/JMLr2iT3AoY1mwChglXjhYy66cFci644+ZF90QbQ2LaqqX+8qGEMIjFzcLCXiGX2/bbpep7HafrN2aHmc1fOGkT2ao12B6hYyieYTkiIj3t2Mjw8x+Dx6nbqkWBbLcUZ3C2/YQfQ2LNBRyrSQfinKoEjqe9xmeyn5XxAEswTEdIVFDtE63ABkiM2UvBABuMHEq4Nz/no0Ec4fOP0Tdypkl3zdRpIyObjrzt2WlAEc26NeqCqNdDftkBI7oV/9nxxsYDFSILl7h+D6jVph2W8Ent4qeaA+O4HqiGlJNmZ7DSPwU4+fHgnfJCfN+2oL13GqohPA=="
 
-	myLat float64
-	myLon float64
-
 	interestingMMSI = map[string]bool{
 		"338691000": true, // Matthew Turner
 		"338359786": true, // Randy S Cummings (associated with Pacific Responder?)
@@ -137,6 +134,11 @@ func lookAtShips(latA, lonA, latB, lonB float64) {
 			break
 		}
 
+		// Only alert for ships visible from our apartment.
+		if !visibleFromApt(details.Lat, details.Lon) {
+			continue
+		}
+
 		// Only alert for ships that are moving.
 		if details.Speed < 2.0 {
 			continue
@@ -150,11 +152,6 @@ func lookAtShips(latA, lonA, latB, lonB float64) {
 			}
 		}
 
-		// Only alert for ships visible from our apartment.
-		if !visibleFromApt(details.Lat, details.Lon) {
-			continue
-		}
-
 		// If we have recently seen this ship, skip it.
 		now := time.Now().Unix()
 		elapsed := now - database.LookupLastSighting(details)
@@ -165,6 +162,7 @@ func lookAtShips(latA, lonA, latB, lonB float64) {
 		}
 
 		// We have passed all the tests! Save and alert.
+		myLat, myLon := myGeo()
 		database.SaveSighting(details, myLat, myLon)
 		err := alert.Alert(details)
 		if err != nil {
@@ -192,27 +190,11 @@ func box(lat, lon float64, nmiles float64) (latA, lonA, latB, lonB float64) {
 
 // scanNearby continually scans for ships within a given radius of this computer.
 func scanNearby(sleepSecs time.Duration) {
-	// TODO: If the bounding region of 'nearby' overlaps the bounding
-	// region of scan_apt_visible then do not scan 'nearby'.
-	lat, lon := myGeo()
-	latA, lonA, latB, lonB := box(lat, lon, 30)
-
-	// Open channel.
-	c := make(chan database.Ship, 10)
-
 	for {
-		go vesselfinder.ShipsInRegion(latA, lonA, latB, lonB, c)
+		lat, lon := myGeo()
+		latA, lonA, latB, lonB := box(lat, lon, 30)
 
-		// Read from channel.
-		for {
-			_, ok := <-c
-			if !ok {
-				break
-			}
-
-			// TODO: Add alerting to notify ships are near.
-		}
-
+		lookAtShips(latA, lonA, latB, lonB)
 		time.Sleep(sleepSecs)
 	}
 }
@@ -231,35 +213,22 @@ func scanAptVisible(sleepSecs time.Duration) {
 // scanPlanet continually scans the entire planet for heretofore unseen ships.
 func scanPlanet(sleepSecs time.Duration) {
 	for {
-		// Pick a random lat/lon box on the surface of the planet.
+		// Pick a random lat/lon box of size 'step' on the surface of the planet.
 		step := 10
 		latA := float64(rand.Intn(360-step) - 180)
 		lonA := float64(rand.Intn(360-step) - 180)
-
 		latB := latA + float64(step)
 		lonB := lonA + float64(step)
 
-		// Open channel.
-		c := make(chan database.Ship, 10)
-
-		go vesselfinder.ShipsInRegion(latA, lonA, latB, lonB, c)
-
-		// Read from channel.
-		for {
-			_, ok := <-c
-			if !ok {
-				break
-			}
-		}
-
+		lookAtShips(latA, lonA, latB, lonB)
 		time.Sleep(sleepSecs)
 	}
 }
 
 // tides looks up instantaneous tide data for a given NOAA station.
-func tides(sleepSecs time.Duration) {
+func tides(sleepSecs time.Duration, station string) {
 	for {
-		reading, ok := noaa.Tides("9414290")
+		reading, ok := noaa.Tides(station)
 		if ok {
 			fmt.Println("Reading:", reading)
 		}
@@ -268,9 +237,9 @@ func tides(sleepSecs time.Duration) {
 }
 
 // airGap looks up instantaneous air gap (distance from bottom of bridge to water) for a given NOAA station.
-func airGap(sleepSecs time.Duration) {
+func airGap(sleepSecs time.Duration, station string) {
 	for {
-		reading, ok := noaa.AirGap("9414304")
+		reading, ok := noaa.AirGap(station)
 		if ok {
 			fmt.Println("Reading:", reading)
 		}
@@ -313,13 +282,11 @@ func Start(passPhrase string) error {
 		return err
 	}
 
-	myLat, myLon = myGeo()
-
 	// go scanNearby(5 * 60 * time.Second)
 	go scanAptVisible(2 * 60 * time.Second)
 	go scanPlanet(2 * 60 * time.Second)
-	go tides(10 * 60 * time.Second)
-	go airGap(10 * 60 * time.Second)
+	go tides(10*60*time.Second, "9414290")
+	go airGap(10*60*time.Second, "9414304")
 	go dbStats(10 * 60 * time.Second)
 
 	return nil
